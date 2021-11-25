a426c15b883c4eb33346452412909bf7b831ed2a
sophiea0110 <sophiea0110@naver.com>
2021-11-26 00:11:18 +0900

    Todo 리스트 구현중2

--- src/Component/Clock.js
+++ src/Component/Clock.js
@@ -6,13 +6,14 @@ function Clock(){
     const [tick, setTick] = useState('')
 
     function CurrnetClock() {
+        
         const date = new Date()
 
         const hours = String(date.getHours()).padStart(2,"0")
         const minutes = String(date.getMinutes()).padStart(2,"0")
         const sec = String(date.getSeconds()).padStart(2,"0")
 
-        const currentClock = `${hours} : ${minutes} : ${sec}`
+        const currentClock = `${hours} : ${minutes} : ${sec} `
 
         setTick(currentClock)
 
--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 
+function TodoPaint(){
+
+    const savedToDos = localStorage.getItem("todolist")
+    const parsedToDos = JSON.parse(savedToDos)
+
+    return (
+        <div>
+            {parsedToDos.map(todos => (
+            <div>
+                <span>{todos.text}</span>
+                <button>삭제</button>
+            </div>  
+            ))}
+        </div>
+    )
+}
 
 
 function Todo(){
+
+    const [TodoList, SetTodoList] = useState([{
+        text : '',
+        id : '',
+        result : ''
+    }])
 
     const InputStyle = {
         borderTop: "none",
@@ -16,8 +38,26 @@ function Todo(){
 
     function handleToDoSubmit(e){
         e.preventDefault();
-        const todo = e.target.children[0]
-        console.log(todo.value)
+        const taget = e.target.children[0]
+        
+        const todo = {
+            text : taget.value,
+            id : Date.now(),
+            result : "false"
+        }
+
+        SetTodoList([...TodoList, todo])
+
+        console.log(TodoList)
+
+        saveTodoList()
+
+        taget.value = ''
+
+    }
+
+    function saveTodoList(){
+        localStorage.setItem('todolist', JSON.stringify(TodoList))
     }
 
     return(
@@ -25,7 +65,7 @@ function Todo(){
             <form onSubmit={handleToDoSubmit}>
                 <input type="text" style={InputStyle} placeholder="Write a To Do and Press Enter" />
             </form>
-
+            <TodoPaint/>
         </div>
     )
 }
